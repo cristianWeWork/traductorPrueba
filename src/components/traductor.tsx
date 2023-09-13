6112ce2de02c8f7d2f73a0c877dd522e19ed9e6b
Cristian Maciel~ <cristian@weworkfactory.com>
2023-09-13 11:51:52 +0200

    a1

---
+++ src/components/traductor.tsx
@@ -0,0 +1,61 @@
+import React, { useState, ChangeEvent } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
+import './css/traductorStyle.css'
+import axios from "axios";
+const TraductorUrl = "https://we-work-prueba.azurewebsites.net/translateMe/"
+
+const Traductor: React.FC = () => {
+    const [inputValue, setInputValue] = useState<string>('');
+    const [respuesta, setRespuesta] = useState<any>();
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleEnviarClick = () => {
+        axios
+            .post(TraductorUrl, {
+                text: inputValue
+            })
+            .then((response: any) => {
+                setRespuesta(response.translations)
+            });
+    };
+
+    return (
+        <Container className='container'>
+            <div className='center'>
+                <Row>
+                    <Col>
+                        <h1>Traductor</h1>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="Escribe algo..."
+                                value={inputValue}
+                                onChange={handleInputChange}
+                            />
+                            <Button variant="primary" onClick={handleEnviarClick}>
+                                Enviar
+                            </Button>
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="mt-3">
+                            <h3>Respuesta:</h3>
+                            <div className="border p-3">{respuesta?.map( (translate:any) =>{
+                                <div>
+                                    <p>En {translate.to} : {translate.text}</p> 
+                                </div>
+                            })}</div>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        </Container>
+    );
+};
+
+export default Traductor;
